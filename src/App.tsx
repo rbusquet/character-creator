d7c8d935834c8aeae064472663bf8399fa030016
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2021-03-27 17:49:07 -0400

    final solution

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,15 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
+      <div
+        style={{
+          position: "fixed",
+          width: "100%",
+          top: "60%",
+          height: "100%",
+          backgroundColor: "hsl(195deg, 20%, 86%)",
+        }}
+      ></div>
       <CharacterEditor />
       <Footer />
     </>
