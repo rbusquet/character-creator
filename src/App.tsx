0832a398e15e618e6c4a2a1e3bbaf1d7216ba6a1
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2021-03-28 21:46:49 -0400

    adjust scroll. perspective as a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import CharacterEditor from "./components/CharacterEditor";
 import Footer from "./components/Footer";
+import Perspective from "./components/Perspective";
 
 function App() {
   return (
     <>
-      <div
-        style={{
-          position: "fixed",
-          width: "100%",
-          top: "60%",
-          height: "100%",
-          backgroundColor: "hsl(195deg, 20%, 86%)",
-        }}
-      ></div>
+      <Perspective />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/Perspective/Perspective.tsx
@@ -0,0 +1,5 @@
+import styles from "./Perspective.module.css";
+
+export default function Perspective() {
+  return <div className={styles.perspective} />;
+}
